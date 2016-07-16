2cf3a6961be789e28072ce2b632146bb9e79a2bb
naridas <gregsparta@gmail.com>
2016-07-16 22:44:52 +0100

    TDD promo_rules calculations/changes might need to extract this out into another class

--- lib/promo_rules.rb
+++ lib/promo_rules.rb
@@ -1,19 +1,44 @@
 class PromoRules
-  PROMO_1 = 60
-  PROMO_2 = 2
+  PROMO_60 = 60
+  PROMO_001 = 2
+  TEN_PERCENT_OFF = 0.9
 
-	def two_plus_001?(array)
-		array.count{|product| product_001?(product)} >= PROMO_2
+  def change_price_promo_001(basket)
+    return change_price_001(basket) if two_plus_001?(basket)
+    basket
+  end
+
+  def calculate_promo_60(basket)
+    return ten_percent_off_total(basket) if over60?(basket)
+    basket_sum(basket)
+  end
+
+	def two_plus_001?(basket)
+		basket.count{|product| product_001?(product)} >= PROMO_001
 	end
 
-  def over60?(array)
-    array.inject(0){|sum,product| sum + product.price } > PROMO_1
+  def change_price_001(basket)
+    basket.map do |product|
+      product.price = 8.50 if product_001?(product)
+      product
+    end
+  end
+
+  def over60?(basket)
+    basket_sum(basket) > PROMO_60
+  end
+
+  def ten_percent_off_total(basket)
+    basket_sum(basket) * TEN_PERCENT_OFF
   end
 
   private
 
+  def basket_sum(basket)
+    basket.inject(0){|sum,product| sum + product.price }
+  end
+
   def product_001?(product)
     product.product_code == 001
   end
-
 end
--- spec/promo_rules_spec.rb
+++ spec/promo_rules_spec.rb
@@ -4,17 +4,65 @@ describe PromoRules do
   let(:product_001) { double :product, price:9.25, product_code:001}
   let(:product_002) { double :other_station, price:45.00, product_code:002 }
   subject(:promo_rules) { described_class.new() }
-  context "If you have 2+ lavender hearts" do
+  context "Promo Rules 2+ lavender hearts" do
     it "2+ lavender hearts return true" do
-      array = [product_001, product_001]
-      expect(promo_rules.two_plus_001?(array)).to eq true
+      basket = [product_001, product_001]
+      expect(promo_rules.two_plus_001?(basket)).to eq true
+    end
+
+    it "less than 2 lavender hearts return false" do
+      basket = [product_002, product_001]
+      expect(promo_rules.two_plus_001?(basket)).to eq false
+    end
+
+    it "change price of lavender hearts" do
+      basket = [product_001]
+      allow(product_001).to receive(:price=)
+      allow(product_001).to receive(:price).and_return(8.50)
+      expect(promo_rules.change_price_001(basket)).to eq [product_001]
+      expect(product_001.price).to eq 8.50
+    end
+
+    it "change price of lavender hearts if 2+ lavender hearts" do
+      basket = [product_001, product_001, product_002]
+      allow(product_001).to receive(:price=)
+      allow(product_001).to receive(:price).and_return(8.50)
+      expect(promo_rules.change_price_001(basket)).to eq [product_001, product_001, product_002]
+      expect(product_001.price).to eq 8.50
+    end
+
+    it "less than 2 lavender hearts return basket" do
+      basket = [product_001, product_002]
+      allow(product_001).to receive(:price=)
+      expect(promo_rules.change_price_001(basket)).to eq [product_001, product_002]
+      expect(product_001.price).to eq 9.25
     end
   end
 
-  context "If you have price greater than 60 " do
+  context "Promo Rules over £60" do
     it "over £60 return true" do
-      array = [product_002, product_002]
-      expect(promo_rules.over60?(array)).to eq true
+      basket = [product_002, product_002]
+      expect(promo_rules.over60?(basket)).to eq true
+    end
+
+    it "over £60 return false" do
+      basket = [product_002]
+      expect(promo_rules.over60?(basket)).to eq false
+    end
+
+    it "ten percent off" do
+      basket = [product_002]
+      expect(promo_rules.ten_percent_off_total(basket)).to eq 40.5
+    end
+
+    it "calculates new total price if over £60" do
+      basket = [product_002, product_002]
+      expect(promo_rules.calculate_promo_60(basket)).to eq 81
+    end
+
+    it "returns normal total price if less then £60" do
+      basket = [product_002]
+      expect(promo_rules.calculate_promo_60(basket)).to eq 45
     end
   end
 
