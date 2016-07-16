d0a15bd75822aa01a32fa5ebb1fb126f73ae7629
naridas <gregsparta@gmail.com>
2016-07-16 23:00:05 +0100

    fixes in test and hound fixes

--- spec/promo_rules_spec.rb
+++ spec/promo_rules_spec.rb
@@ -3,7 +3,7 @@ require 'promo_rules'
 describe PromoRules do
   let(:product_001) { double :product, price:9.25, product_code:001}
   let(:product_002) { double :other_station, price:45.00, product_code:002 }
-  subject(:promo_rules) { described_class.new() }
+  subject(:promo_rules) { described_class.new }
   context "Promo Rules 2+ lavender hearts" do
     it "2+ lavender hearts return true" do
       basket = [product_001, product_001]
@@ -27,14 +27,14 @@ describe PromoRules do
       basket = [product_001, product_001, product_002]
       allow(product_001).to receive(:price=)
       allow(product_001).to receive(:price).and_return(8.50)
-      expect(promo_rules.change_price_001(basket)).to eq [product_001, product_001, product_002]
+      expect(promo_rules.change_price_promo_001(basket)).to eq [product_001, product_001, product_002]
       expect(product_001.price).to eq 8.50
     end
 
     it "less than 2 lavender hearts return basket" do
       basket = [product_001, product_002]
       allow(product_001).to receive(:price=)
-      expect(promo_rules.change_price_001(basket)).to eq [product_001, product_002]
+      expect(promo_rules.change_price_promo_001(basket)).to eq [product_001, product_002]
       expect(product_001.price).to eq 9.25
     end
   end
